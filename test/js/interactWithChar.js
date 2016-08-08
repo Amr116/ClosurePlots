c3e97748e13718b9117db743e1427c06c7208bbd
Amr Elsayed <amr11682@hotmail.com>
2016-08-08 23:51:09 +0200

    some different update

--- test/js/interactWithChar.js
+++ test/js/interactWithChar.js
@@ -33,3 +33,21 @@ function firstStage(){
 	var chart = new google.charts.Scatter(document.getElementById('test1'));
 	chart.draw(data, google.charts.Scatter.convertOptions(options));
 }
+
+function toFixed(x) {
+    var result = '';
+    var xStr = x.toString(10);
+    var digitCount = xStr.indexOf('e') === -1 ? xStr.length : (parseInt(xStr.substr(xStr.indexOf('e') + 1)) + 1);
+
+    for (var i = 1; i <= digitCount; i++) {
+        var mod = (x % Math.pow(10, i)).toString(10);
+        var exponent = (mod.indexOf('e') === -1) ? 0 : parseInt(mod.substr(mod.indexOf('e')+1));
+        if ((exponent === 0 && mod.length !== i) || (exponent > 0 && exponent !== i-1)) {
+            result = '0' + result;
+        }
+        else {
+            result = mod.charAt(0) + result;
+        }
+    }
+    return result;
+}
